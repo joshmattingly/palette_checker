82dd4c0362013f7c6ed192dc918a529e322febe0
Josh Mattingly <josh.mattingly@gmail.com>
2019-02-26 20:19:35 -0600

    aligned grid

--- palette_checker.js
+++ palette_checker.js
@@ -10,21 +10,19 @@ var svg = d3.select("body").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var squareSide = 50;
+var squareSide = 30;
 
-var testData = ["#2d9b68", "#f2f2ed", "#afba5b", "#196d51", "#43C5E4", "#E82C2A", "#62BB46", "#BFB8BF", "#231F20",
-    "#C3271B", "#EA5329", "#007EC5", "#94D8E9", "#007933", "#A5CF4F"];
+var testData = ['#a50026','#d73027','#f46d43','#fdae61','#fee090','#ffffbf','#e0f3f8','#abd9e9','#74add1','#4575b4','#313695',
+    '#543005','#8c510a','#bf812d','#dfc27d','#f6e8c3','#f5f5f5','#c7eae5','#80cdc1','#35978f','#01665e','#003c30'];
 
 
 /* TODO: Change scales to grow and shrink with dataset */
-var xScale = d3.scaleOrdinal()
-    .domain(testData)
-    .range([42, 84, 126, 168, 210, 252])
+var gridScale = d3.scaleOrdinal()
+    .range([42,84,126,168,210,252,294,336,378,420])
 
-var yScale = d3.scaleOrdinal()
-    .domain(testData)
-    .range([42, 84, 126, 168, 210, 252]);
-
+var maxColumns = 10;
+var rowCounter = 2;
+var columnCounter = 0;
 
 var colorSwatch = svg.selectAll('rect')
     .data(testData)
@@ -34,8 +32,54 @@ var colorSwatch = svg.selectAll('rect')
     .attr("class", "swatch")
     .attr("width", squareSide)
     .attr("height", squareSide)
-    .attr("x", function(d,i){ return xScale(i); })
-    .attr("y", function(d,i){ return yScale(i); })
-    .attr("fill", function(d){
-        return d;
-    });
+    .attr("x", function(){
+        if(columnCounter < maxColumns){
+            columnCounter++;
+            return gridScale(columnCounter);
+        } else {
+            columnCounter = 0;
+            return gridScale(columnCounter);
+        }
+    })
+    .attr("y", function(){
+        if(columnCounter < maxColumns){
+            columnCounter++;
+            return gridScale(rowCounter);
+        } else {
+            columnCounter = 0;
+            rowCounter++;
+            return gridScale(rowCounter);
+        }
+    })
+    .attr("fill", function(d){ return d; });
+
+columnCounter = 0;
+rowCounter = 2;
+
+var swatchText = svg.selectAll("text")
+    .data(testData)
+    .enter()
+    .append("g")
+    .append("text")
+    .attr("font-size", "9px")
+    .text(function(d){ return d; })
+    .attr("x", function(d,i){
+        if(columnCounter < maxColumns){
+            columnCounter++;
+            return gridScale(columnCounter);
+        } else {
+            columnCounter = 0;
+            return gridScale(columnCounter);
+        }
+    })
+    .attr("y", function(){
+        if(columnCounter < maxColumns){
+            columnCounter++;
+            return gridScale(rowCounter);
+        } else {
+            columnCounter = 0;
+            rowCounter++;
+            return gridScale(rowCounter);
+        }
+    })
+    .text(function(d){ return d; });
