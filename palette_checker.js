6f25567cbd84ff1f236ea690c848a0322974b8c6
Josh Mattingly <josh.mattingly@gmail.com>
2019-02-26 22:15:08 -0600

    fixed shifting on new data

--- palette_checker.js
+++ palette_checker.js
@@ -1,33 +1,34 @@
+var margin = {top: 20, right: 10, bottom: 20, left: 10};
+
+var width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+var squareSide = 60;
+
+var gridScale = d3.scaleOrdinal()
+    .range([84, 168, 252, 336, 420, 504, 588, 672, 756, 840]);
+
+
+
 function show() {
-    var form = document.getElementById("theForm");
-    var inputString = form.elements.color_palette.value.replace(/[\[\]']+/g,"");
-
-    var dataset = inputString.split(',');
-
-    var margin = {top: 20, right: 10, bottom: 20, left: 10};
-
-    var width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-
-    var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var squareSide = 60;
-
-    var gridScale = d3.scaleOrdinal()
-        .range([84, 168, 252, 336, 420, 504, 588, 672, 756, 840]);
+    d3.selectAll("g > *").remove();
 
     var maxColumns = 10;
     var rowCounter = 2;
     var columnCounter = 0;
 
+    svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    var form = document.getElementById("theForm");
+    var inputString = form.elements.color_palette.value.replace(/[\[\]']+/g,"");
+
+    var dataset = inputString.split(',');
     var colorSwatch = svg.selectAll('rect')
-        .exit()
-        .remove()
         .data(dataset)
         .enter()
         .append("g")
